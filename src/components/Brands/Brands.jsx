a8f01d89be15e61e05bb0d57a32a1a46008eb5c4
Harry Kennedy <72253270+Harrythedevelopercs@users.noreply.github.com>
2024-07-23 07:51:05 +0500

    websites links added

--- src/components/Brands/Brands.jsx
+++ src/components/Brands/Brands.jsx
@@ -29,7 +29,7 @@ const Brands = () => {
                         <p className='text-white text-[17px] lg:text-[20px] font-sans font-[500] my-2'>Elevate Your Written Works to New Heights</p>
                         <h5 className='text-[#438EFF] font-sans text-[20px] lg:text-[23px] font-[500] '>BookWritingCube.com</h5>
                         <p className='font-sans text-white text-[14px] lg:text-[16px] mt-3 mb-8 lg:pr-5 h-[100px] overflow-y-scroll lg:overflow-y-hidden lg:h-full'>BookWritingCube, a flagship service of Bhaoo Inc, brings your creative ideas to life through professional writing services. Whether you are an aspiring author, academic researcher, or business professional, our talented team of writers and editors are dedicated to delivering exceptional written content that captivates and engages your audience. From book ghostwriting to editing and proofreading, we ensure your written works are polished, impactful, and ready to make a lasting impression.</p>
-                        <Link href="#" className='border-white border-2 text-white font-sans text-[16px] rounded-[12px] px-[30px] py-[8px] flex items-center w-fit gap-x-3 group'>
+                        <Link href="https://bookwritingcube.com" target="_blank" className='border-white border-2 text-white font-sans text-[16px] rounded-[12px] px-[30px] py-[8px] flex items-center w-fit gap-x-3 group'>
                             Learn More
                              <FaArrowRight className='group-hover:ml-[20px] duration-700 group-hover:duration-700 ease-in-out' />
                         </Link>
@@ -47,7 +47,7 @@ const Brands = () => {
                         <p className='text-white text-[17px] lg:text-[20px] font-sans font-[500] my-2'>Unleash the Power of Custom Software Solutions</p>
                         <h5 className='text-[#438EFF] font-sans text-[20px] lg:text-[23px] font-[500] '>BitsWits.co</h5>
                         <p className='font-sans text-white text-[14px] lg:text-[16px] mt-3 mb-8 lg:pr-5 h-[100px] overflow-y-scroll lg:overflow-y-hidden lg:h-full'>BitsWits, a trusted division of Bhaoo Inc, specializes in custom software development and IT consulting services. Our team of expert developers and consultants collaborates closely with clients to understand their unique business requirements and design bespoke software solutions. From web and mobile app development to cloud integration and automation, we empower businesses to leverage cutting-edge technology and enhance operational efficiency, productivity, and growth.</p>
-                        <Link href="#" className='border-white border-2 text-white font-sans text-[16px] rounded-[12px] px-[30px] py-[8px] flex items-center w-fit gap-x-3 group'>
+                        <Link href="https://bitswits.co" target="_blank" className='border-white border-2 text-white font-sans text-[16px] rounded-[12px] px-[30px] py-[8px] flex items-center w-fit gap-x-3 group'>
                             Learn More
                              <FaArrowRight className='group-hover:ml-[20px] duration-700 group-hover:duration-700 ease-in-out' />
                         </Link>
@@ -59,7 +59,7 @@ const Brands = () => {
                         <p className='text-white text-[17px] lg:text-[20px] font-sans font-[500] my-2'>Embrace the World of Digital Collectibles</p>
                         <h5 className='text-[#438EFF] font-sans text-[20px] lg:text-[23px] font-[500] '>NFTKreator.com</h5>
                         <p className='font-sans text-white text-[14px] lg:text-[16px] mt-3 mb-8 lg:pr-5 h-[100px] overflow-y-scroll lg:overflow-y-hidden lg:h-full'>NFT Kreator, an innovative venture under the umbrella of Bhaoo Inc, enables artists, creators, and collectors to embrace the world of non-fungible tokens (NFTs). Our platform facilitates the creation, showcasing, and monetization of unique digital assets. With a secure blockchain foundation, intuitive user interface, and a thriving community, NFT Kreator empowers individuals and businesses to explore new revenue streams and participate in the growing digital economy.</p>
-                        <Link href="#" className='border-white border-2 text-white font-sans text-[16px] rounded-[12px] px-[30px] py-[8px] flex items-center w-fit gap-x-3 group'>
+                        <Link href="https://nftkreator.com" target="_blank" className='border-white border-2 text-white font-sans text-[16px] rounded-[12px] px-[30px] py-[8px] flex items-center w-fit gap-x-3 group'>
                             Learn More
                              <FaArrowRight className='group-hover:ml-[20px] duration-700 group-hover:duration-700 ease-in-out' />
                         </Link>
@@ -77,7 +77,7 @@ const Brands = () => {
                         <p className='text-white text-[17px] lg:text-[20px] font-sans font-[500] my-2'>Propel Your Online Presence to New Heights</p>
                         <h5 className='text-[#438EFF] font-sans text-[20px] lg:text-[23px] font-[500] '>SEOResultsPro.com</h5>
                         <p className='font-sans text-white text-[14px] lg:text-[16px] mt-3 mb-8 lg:pr-5 h-[100px] overflow-y-scroll lg:overflow-y-hidden lg:h-full'>SEO Results Pro, a prominent offering by Bhaoo Inc, is a results-driven digital marketing agency. Our specialized search engine optimization (SEO) services boost your online presence, drive organic traffic, and generate valuable leads. With a team of experienced professionals and advanced strategies, we optimize your website’s visibility, improve search engine rankings, and help you stand out in the highly competitive digital landscape.</p>
-                        <Link href="#" className='border-white border-2 text-white font-sans text-[16px] rounded-[12px] px-[30px] py-[8px] flex items-center w-fit gap-x-3 group'>
+                        <Link href="https://seoresultspro.com" target="_blank" className='border-white border-2 text-white font-sans text-[16px] rounded-[12px] px-[30px] py-[8px] flex items-center w-fit gap-x-3 group'>
                             Learn More
                              <FaArrowRight className='group-hover:ml-[20px] duration-700 group-hover:duration-700 ease-in-out' />
                         </Link>
@@ -89,7 +89,7 @@ const Brands = () => {
                         <p className='text-white text-[17px] lg:text-[20px] font-sans font-[500] my-2'>Accelerate Your Business Growth with Strategic Marketing</p>
                         <h5 className='text-[#438EFF] font-sans text-[20px] lg:text-[23px] font-[500] '>LeadzExperts.com</h5>
                         <p className='font-sans text-white text-[14px] lg:text-[16px] mt-3 mb-8 lg:pr-5 h-[100px] overflow-y-scroll lg:overflow-y-hidden lg:h-full'>Leadz Experts, a strategic partner of Bhaoo Inc, is a results-oriented digital marketing agency. Our comprehensive marketing solutions drive lead generation, customer engagement, and business growth. From strategic planning and implementation to marketing automation and analytics, we help businesses maximize their marketing ROI and achieve sustainable success in the ever-evolving digital landscape.</p>
-                        <Link href="#" className='border-white border-2 text-white font-sans text-[16px] rounded-[12px] px-[30px] py-[8px] flex items-center w-fit gap-x-3 group'>
+                        <Link href="https://leadexperts.com" target="_blank" className='border-white border-2 text-white font-sans text-[16px] rounded-[12px] px-[30px] py-[8px] flex items-center w-fit gap-x-3 group'>
                             Learn More
                              <FaArrowRight className='group-hover:ml-[20px] duration-700 group-hover:duration-700 ease-in-out' />
                         </Link>
@@ -107,7 +107,7 @@ const Brands = () => {
                         <p className='text-white text-[17px] lg:text-[20px] font-sans font-[500] my-2'>Unleashing Imagination, Inspiring Generations</p>
                         <h5 className='text-[#438EFF] font-sans text-[20px] lg:text-[23px] font-[500] '>bestsellingpublisher.com</h5>
                         <p className='font-sans text-white text-[14px] lg:text-[16px] mt-3 mb-8 lg:pr-5 h-[100px] overflow-y-scroll lg:overflow-y-hidden lg:h-full'>As the premier destination for best-selling literature, we’re more than just a book writing, editing, and publishing powerhouse. We’re a dynamic fusion of creativity and innovation, brought to life by a dedicated team of writers, editors, publishers, developers, and designers. Together, we don’t just produce books; we craft unforgettable journeys, market masterpieces, and ignite imaginations worldwide. Welcome to where stories transcend pages, shaping the future of literature.</p>
-                        <Link href="#" className='border-white border-2 text-white font-sans text-[16px] rounded-[12px] px-[30px] py-[8px] flex items-center w-fit gap-x-3 group'>
+                        <Link href="https://bestsellingpublisher.com" target="_blank" className='border-white border-2 text-white font-sans text-[16px] rounded-[12px] px-[30px] py-[8px] flex items-center w-fit gap-x-3 group'>
                             Learn More
                              <FaArrowRight className='group-hover:ml-[20px] duration-700 group-hover:duration-700 ease-in-out' />
                         </Link>
@@ -119,7 +119,7 @@ const Brands = () => {
                         <p className='text-white text-[17px] lg:text-[20px] font-sans font-[500] my-2'>Let Our Animations Tell Your Story</p>
                         <h5 className='text-[#438EFF] font-sans text-[20px] lg:text-[23px] font-[500] '>infinityanimations.com</h5>
                         <p className='font-sans text-white text-[14px] lg:text-[16px] mt-3 mb-8 lg:pr-5 h-[100px] overflow-y-scroll lg:overflow-y-hidden lg:h-full'>Transforming Visions into Vivid Realities: Elevate Your Storytelling with Our Animation Magic. Whether you’re a brand looking to mesmerize audiences, an educator aiming to demystify complexities, or a content creator chasing visual perfection, our animation services are your canvas. We merge technical mastery with artistic genius, infusing life into every frame and creating animations that defy expectations. Let us turn your ideas into animated masterpieces that captivate, educate, and inspire.</p>
-                        <Link href="#" className='border-white border-2 text-white font-sans text-[16px] rounded-[12px] px-[30px] py-[8px] flex items-center w-fit gap-x-3 group'>
+                        <Link href="https://infinityanimations.com" target="_blank" className='border-white border-2 text-white font-sans text-[16px] rounded-[12px] px-[30px] py-[8px] flex items-center w-fit gap-x-3 group'>
                             Learn More
                              <FaArrowRight className='group-hover:ml-[20px] duration-700 group-hover:duration-700 ease-in-out' />
                         </Link>
@@ -137,7 +137,7 @@ const Brands = () => {
                         <p className='text-white text-[17px] lg:text-[20px] font-sans font-[500] my-2'>A Platform Serving Vivid Creatives To Businesses And Marketers</p>
                         <h5 className='text-[#438EFF] font-sans text-[20px] lg:text-[23px] font-[500] '>explainervideosllc.com</h5>
                         <p className='font-sans text-white text-[14px] lg:text-[16px] mt-3 mb-8 lg:pr-5 h-[100px] overflow-y-scroll lg:overflow-y-hidden lg:h-full'>As a prominent 2D Animation Company based in the United States, we recognize that achieving a level of proficiency is insufficient in the current fiercely competitive market landscape. In an era where customers are no longer content with mundane transactions, they yearn for a level of engagement that transcends the ordinary; they seek unparalleled experiences from the brands they interact with.</p>
-                        <Link href="#" className='border-white border-2 text-white font-sans text-[16px] rounded-[12px] px-[30px] py-[8px] flex items-center w-fit gap-x-3 group'>
+                        <Link href="https://explainervideosllc.com" target="_blank" className='border-white border-2 text-white font-sans text-[16px] rounded-[12px] px-[30px] py-[8px] flex items-center w-fit gap-x-3 group'>
                             Learn More
                              <FaArrowRight className='group-hover:ml-[20px] duration-700 group-hover:duration-700 ease-in-out' />
                         </Link>
